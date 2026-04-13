33388ae5ad206ed847847a80e63627a7b1a28546
duwenbin0316 <duwenbin0316@users.noreply.github.com>
2026-04-13 19:27:33 +0800

    test human modified code

--- react-router5-demo/src/components/CounterCard.jsx
+++ react-router5-demo/src/components/CounterCard.jsx
@@ -12,6 +12,11 @@ function CounterCard({ title = '计数器示例', initial = 0 }) {
         <button className="solid-button" onClick={() => setCount((c) => c + 1)}>+</button>
         <button className="ghost-button" onClick={() => setCount(initial)}>重置</button>
       </div>
+      <div>
+        <div>
+          just for test
+        </div>
+      </div>
     </article>
   )
 }
